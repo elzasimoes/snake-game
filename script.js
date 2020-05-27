2bef0f99abee42c06afdda7a1ea22526d61c9287
Elza Simões <elzaesimoes@gmail.com>
2020-05-26 23:23:21 -0400

    Creating snake movements #02

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 let canvas = document.getElementById('snake');
-let context = canvas.getContext("2d"); 
+let context = canvas.getContext("2d");
 let box = 32;
 let snake = [];
 
@@ -16,33 +16,42 @@ function createBG() {
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
-function createSnake(){
-    for(i=0; i < snake.length; i++){
+function createSnake() {
+    for (i = 0; i < snake.length; i++) {
         context.fillStyle = "green";
-        context.fillRect(snake[i].x, snake[i].y, box, box);        
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-function startGame(){
+document.addEventListener('keydown', update);
+
+function update (event) {
+    if (event.keyCode == 37 && direction != "right") direction = "left";
+    if (event.keyCode == 38 && direction != "down") direction = "up";
+    if (event.keyCode == 39 && direction != "left") direction = "right";
+    if (event.keyCode == 40 && direction != "up") direction = "down";
+}
+
+function startGame() {
     createBG();
     createSnake();
-    
-     let snakeX = snake[0].x;
-     let snakeY = snake[0].y;
 
-     if(direction == "right") snakeX += box;
-     if(direction == "left") snakeY -= box;
-     if(direction == "up") snakeY -= box;
-     if(direction == "down") snakeY -= box;
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
 
-        snake.pop();
+    if (direction == "right") snakeX += box;
+    if (direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if (direction == "down") snakeY += box;
 
-        let newHead ={
-            x: snakeX,
-            y: snakeY
-        }
+    snake.pop();
 
-        snake.unshift(newHead);
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
 
 }
 
