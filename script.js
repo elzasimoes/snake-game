d138c6c4fdb4c575e21a1975e55281018a373ebf
Elza Simões <elzaesimoes@gmail.com>
2020-05-26 23:11:43 -0400

    Creating snake movements

--- script.js
+++ script.js
@@ -9,7 +9,9 @@ snake[0] = {
     y: 8 * box
 }
 
-function criarBG() {
+let direction = "right";
+
+function createBG() {
     context.fillStyle = "lightgreen";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
@@ -17,9 +19,34 @@ function criarBG() {
 function createSnake(){
     for(i=0; i < snake.length; i++){
         context.fillStyle = "green";
-        context.fillRect(snake[i].x, snake[i].y, box, box);
+        context.fillRect(snake[i].x, snake[i].y, box, box);        
     }
 }
 
-criarBG();
-createSnake();
+function startGame(){
+    createBG();
+    createSnake();
+    
+     let snakeX = snake[0].x;
+     let snakeY = snake[0].y;
+
+     if(direction == "right") snakeX += box;
+     if(direction == "left") snakeY -= box;
+     if(direction == "up") snakeY -= box;
+     if(direction == "down") snakeY -= box;
+
+        snake.pop();
+
+        let newHead ={
+            x: snakeX,
+            y: snakeY
+        }
+
+        snake.unshift(newHead);
+
+}
+
+//Dar continuidade ao jogo sem travar
+let game = setInterval(startGame, 100);
+
+
