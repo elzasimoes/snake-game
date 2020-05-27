f04efee8aa5cb349c7a81ba058e11f292d0b17d6
Elza Simões <elzaesimoes@gmail.com>
2020-05-27 00:04:08 -0400

    Snake game completed

--- script.js
+++ script.js
@@ -47,6 +47,12 @@ function startGame() {
     if (snake[0].y > 15 * box && direction == "down") snake[0].y = 0;
     if (snake[0].y < 0 && direction == "up") snake[0].y = 18 * box;
 
+    for (i = 1; i < snake.length; i++) {
+        if (snake[0].x == snake[i].x && snake[0].y == snake[i].y) {
+            clearInterval(game);
+            alert('Game Over :( \n Press F5')
+        }
+    }
     createBackground();
     createSnake();
     snakeFood();
@@ -59,7 +65,13 @@ function startGame() {
     if (direction == "up") snakeY -= box;
     if (direction == "down") snakeY += box;
 
-    snake.pop();
+    if (snakeX != food.x || snakeY != food.y) {
+        snake.pop();
+    } else {
+        food.x = Math.floor(Math.random() * 15 + 1) * box,
+            food.y = Math.floor(Math.random() * 15 + 1) * box
+    }
+
 
     let newHead = {
         x: snakeX,
